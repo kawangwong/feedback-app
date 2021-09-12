1035fc8e1444e554d69f73d55838708afb310f11
kawangwong <nycazn724@gmail.com>
2021-09-12 12:33:42 -0400

    changes

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template, request
+from flask_alchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder="templates-ui")
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/submit', methods = ['POST'])
+def submit():
+    if request.method == "POST":
+        customer = request.form['customer']
+        dealer = request.form['dealer']
+        rating = request.form['rating']
+        comments = request.form['comments']
+        if customer == '' or dealer == '':
+            return render_template("index.html", message = "You did not input the necessary fields")
+        return render_template("success.html")
 
 if __name__ == '__main__':
     app.debug = True ##In dev Flask setting, will referesh
